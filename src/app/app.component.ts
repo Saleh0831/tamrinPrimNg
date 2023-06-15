6000be1620c707171f6bccd9ef68c02a0af1041c
Saleh0831 <105693052+Saleh0831@users.noreply.github.com>
2023-06-15 11:10:10 +0330

    vercelTwTest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product, ProductsResponse } from './product';
+import { AppService } from './app.service';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tamrinPrimNg';
+export class AppComponent implements OnInit {
+  products:Product[]=[];
+  totalRecords:number=0;
+  loading:boolean=true;
+  constructor(private appService:AppService){}
+  ngOnInit(): void {
+    
+  }
+
+  loadProducts(event:LazyLoadEvent){
+    this.loading=true;
+    this.appService.getProducts(event.first || 0).subscribe((response:ProductsResponse)=>{
+      this.loading=false;
+      
+      this.products=response.products;
+      this.totalRecords=response.total;
+    })
+  }
 }
